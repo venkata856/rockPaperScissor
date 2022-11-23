eb53d5fea309b877b1a6932cb2f3bf9afe38e62b
Venkata Bandi <zyenacompany@gmail.com>
2022-11-22 23:05:04 -0500

    added code to play 5 rounds

--- game.js
+++ game.js
@@ -1,8 +1,30 @@
 function getComputerChoice(){
     let computerChoice=["Rock","Paper","Scissors"]
     let random = Math.floor(Math.random() * 3);
-    console.log(random)
     return computerChoice[random]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection,getComputerChoice){
+
+    console.log(getComputerChoice)
+    console.log(playerSelection)
+    if(getComputerChoice.localeCompare(playerSelection)){
+        return "Its a tie Mate!!"
+    }
+    
+}
+
+function play(){
+    for(let i =0; i<5;i++){
+        
+        let playerSelection = prompt("Enter your choice")
+
+        console.log(playRound(playerSelection,getComputerChoice()))
+    }
+}
+
+play();
+
+
+
+// console.log(playRound("rock",getComputerChoice()))
